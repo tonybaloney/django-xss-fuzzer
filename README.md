0fa61ce242983c7c8ca7c4d1a4963b213abeb377
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-07-16 15:24:13 +1000

    initial project checkin

---
+++ README.md
@@ -0,0 +1,3 @@
+# Django XSS Fuzzer
+
+An XSS vulnerability fuzz tester for Django views.
---
+++ django_xss_fuzzer/__init__.py
@@ -0,0 +1,60 @@
+"""
+django-xss-fuzzer: An XSS vulnerability fuzz tester for Django views.
+"""
+
+from random import choice
+
+from django.db.models import Model, QuerySet
+from django.conf import settings
+
+__version__ = '0.1.0'
+
+_DEFAULT_PATTERNS = (
+    '<script>throw onerror=eval,\'=console.log\x28\\\'found script-based xss in {0}\\\'\x29\'</script>',
+    '" onafterscriptexecute=console.log(\'found attribute-based xss in {0}\')')
+
+class ViewFuzzerMiddleware:
+    """
+    Attempts various XSS attacks against the view
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.patterns = getattr(settings, 'XSS_FUZZER_PATTERNS', _DEFAULT_PATTERNS)
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        if request.method not in ('GET',):  # Just GET for now.
+            return None
+
+        # Inject (reflection attack)
+        for key, value in view_kwargs.items():
+            if isinstance(value, str):
+                view_kwargs[key] = self._inject_pattern(key)
+
+    def _reflect_model(self, inst, name):
+        for key, value in inst.__dict__.items():
+            if isinstance(value, str):
+                setattr(inst, key,  self._inject_pattern('{0}.{1}'.format(name, key)))
+
+    def process_template_response(self, request, response):
+        for key, value in response.context_data.items():
+            if key == 'view':
+                continue
+            if isinstance(value, str):
+                response.context_data[key] = self._inject_pattern(key)
+            if isinstance(value, Model):
+                self._reflect_model(value, key)
+            if isinstance(value, QuerySet):
+                # Exhaust lazy query sets so we can hack the attributes
+                _exhausted = list(value)
+                for i in _exhausted:
+                    self._reflect_model(i, key)
+                response.context_data[key] = _exhausted
+
+        return response
+
+    def _inject_pattern(self, key):
+        return choice(self.patterns).format(key)  # nosec
