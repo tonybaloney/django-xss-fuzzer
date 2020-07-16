358e4f161fbf252da7594a3baa429de36a6d345a
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-07-16 15:31:31 +1000

    Update instruction

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Django XSS Fuzzer
 
 An XSS vulnerability fuzz tester for Django views.
+
+## Installation
+
+Install via pip
+
+```console
+$ pip install django-xss-fuzzer
+```
+
+Add `ViewFuzzerMiddleware` to your middleware list for a **test environment**.
+
+```python
+MIDDLEWARE = [
+    ...
+    'django_xss_fuzzer.ViewFuzzerMiddleware'
+]
+```
+
+**Do not deploy this to a production server!**
+
+## Configuration
+
+`XSS_FUZZER_PATTERNS` : A list of XSS patterns to try. See [XSS Cheatsheet](https://portswigger.net/web-security/cross-site-scripting/cheat-sheet) for inspiration.
+`XSS_INJECT_KWARGS` (Default False) : A switch to disable injecting XSS view function keyword arguments
+`XSS_INJECT_CONTEXT_DATA` (Default True) : A switch to disable injecting XSS into class data
--- django_xss_fuzzer/__init__.py
+++ django_xss_fuzzer/__init__.py
@@ -13,6 +13,7 @@ _DEFAULT_PATTERNS = (
     '<script>throw onerror=eval,\'=console.log\x28\\\'found script-based xss in {0}\\\'\x29\'</script>',
     '" onafterscriptexecute=console.log(\'found attribute-based xss in {0}\')')
 
+
 class ViewFuzzerMiddleware:
     """
     Attempts various XSS attacks against the view
@@ -20,6 +21,8 @@ class ViewFuzzerMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         self.patterns = getattr(settings, 'XSS_FUZZER_PATTERNS', _DEFAULT_PATTERNS)
+        self.inject_kwargs = getattr(settings, 'XSS_INJECT_KWARGS', False)
+        self.inject_context_data = getattr(settings, 'XSS_INJECT_CONTEXT_DATA', True)
 
     def __call__(self, request):
         response = self.get_response(request)
@@ -30,9 +33,10 @@ class ViewFuzzerMiddleware:
             return None
 
         # Inject (reflection attack)
-        for key, value in view_kwargs.items():
-            if isinstance(value, str):
-                view_kwargs[key] = self._inject_pattern(key)
+        if self.inject_kwargs:
+            for key, value in view_kwargs.items():
+                if isinstance(value, str):
+                    view_kwargs[key] = self._inject_pattern(key)
 
     def _reflect_model(self, inst, name):
         for key, value in inst.__dict__.items():
@@ -40,8 +44,11 @@ class ViewFuzzerMiddleware:
                 setattr(inst, key,  self._inject_pattern('{0}.{1}'.format(name, key)))
 
     def process_template_response(self, request, response):
+        if not self.inject_context_data:
+            return response
+
         for key, value in response.context_data.items():
-            if key == 'view':
+            if key == 'view':  # ignore this field
                 continue
             if isinstance(value, str):
                 response.context_data[key] = self._inject_pattern(key)
@@ -53,8 +60,12 @@ class ViewFuzzerMiddleware:
                 for i in _exhausted:
                     self._reflect_model(i, key)
                 response.context_data[key] = _exhausted
+            # TODO: inject into other types.
 
         return response
 
     def _inject_pattern(self, key):
+        '''
+        Inject the value as a XSS-attack string with the name of the field inside
+        '''
         return choice(self.patterns).format(key)  # nosec
